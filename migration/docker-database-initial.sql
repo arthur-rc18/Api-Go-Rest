69e4a95bd62075d6fe0d50c4b7ac3bebbcb4c3eb
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-18 11:06:43 -0300

    Sql file responsible for the personality querie

---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,9 @@
+create table personality(
+    id serial primary key,
+    nome varchar,
+    history varchar
+);
+
+INSERT INTO personality(nome, history) VALUES
+('Afonso Augusto Moreira Pena', 'Afonso Augusto Moreira Pena was a lawyer and a brasilian politician. He was the sixth president of Brazil, from 1906 until his death.'),
+('Jose Cesario de Faria Alvim', 'Jose Cesario de Faria Alvim was a lawyer, farmer and economist. He was the president of state of Minas Gerais, from 25 november of 1889 to 10 february of 1890.');
