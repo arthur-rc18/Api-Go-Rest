fda1e68723ce16a1df3f36ce6b1324eb3363b553
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-18 11:08:13 -0300

    Making the connection with the Postgresql database

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+
+	"github.com/arthur-rc18/Api-Go-Rest/database"
+	"github.com/arthur-rc18/Api-Go-Rest/models"
+	"github.com/arthur-rc18/Api-Go-Rest/routes"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf(w, "Home Page")
-}
-
-func HandleRequest() {
-	http.HandleFunc("/", Home)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
-
 func main() {
+
+	models.Personalitys = []models.Personality{
+		{Id: 1, Name: "Name 1", History: "History 1"},
+		{Id: 2, Name: "Name 2", History: "History 2"},
+	}
+
+	database.DbConnection()
+
 	fmt.Println("Starting the Rest server")
-	HandleRequest()
+	routes.HandleRequest()
 }
