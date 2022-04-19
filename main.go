012962543ea2ef9c30c5cc2455d79f1110028e42
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-19 09:20:49 -0300

    Finishing the main

--- main.go
+++ main.go
@@ -4,19 +4,15 @@ import (
 	"fmt"
 
 	"github.com/arthur-rc18/Api-Go-Rest/database"
-	"github.com/arthur-rc18/Api-Go-Rest/models"
+	//"github.com/arthur-rc18/Api-Go-Rest/models"
 	"github.com/arthur-rc18/Api-Go-Rest/routes"
 )
 
 func main() {
 
-	models.Personalitys = []models.Personality{
-		{Id: 1, Name: "Name 1", History: "History 1"},
-		{Id: 2, Name: "Name 2", History: "History 2"},
-	}
-
 	database.DbConnection()
 
 	fmt.Println("Starting the Rest server")
+
 	routes.HandleRequest()
 }
