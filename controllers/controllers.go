da6637ddc70a80db389afaa10f95517c0ed1fa89
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-19 09:21:44 -0300

    Creating and connecting a middleware

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -21,12 +21,38 @@ func AllPersonalitys(w http.ResponseWriter, r *http.Request) {
 }
 
 func ReturnPersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personality models.Personality
+	database.DB.First(&personality, id)
+	json.NewEncoder(w).Encode(personality)
+}
+
+// Function responsible to create a new personality
+func CreateNewPersonality(w http.ResponseWriter, r *http.Request) {
+	var newPersonality models.Personality
+	json.NewDecoder(r.Body).Decode(&newPersonality)
+	database.DB.Create(&newPersonality)
+	json.NewEncoder(w).Encode(newPersonality)
+}
+
+func DeletePersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var personality models.Personality
+	database.DB.Delete(&personality, id)
+	json.NewEncoder(w).Encode(personality)
+}
+
+func EditPersonality(w http.ResponseWriter, r *http.Request) {
 
 	vars := mux.Vars(r)
 	id := vars["id"]
 
 	var personality models.Personality
-
 	database.DB.First(&personality, id)
+	json.NewDecoder(r.Body).Decode(&personality)
+	database.DB.Save(&personality)
 	json.NewEncoder(w).Encode(personality)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -5,16 +5,21 @@ import (
 	"net/http"
 
 	"github.com/arthur-rc18/Api-Go-Rest/controllers"
+	"github.com/arthur-rc18/Api-Go-Rest/middleware"
 	"github.com/gorilla/mux"
 )
 
 // With this function,it is registered a new route with a matcher for the URL path
 func HandleRequest() {
 
+	// NewRouter returns a new router instance
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalitys", controllers.AllPersonalitys).Methods("Get")
-	r.HandleFunc("/api/personalitys/{id}", controllers.ReturnPersonality).Methods("Get")
-
+	r.HandleFunc("/api/personalitys/{id}", controllers.ReturnPersonality).Methods("Get")    // The Get method will receive the data
+	r.HandleFunc("/api/personalitys", controllers.CreateNewPersonality).Methods("Post")     // The Post method will post the data
+	r.HandleFunc("/api/personalitys/{id}", controllers.DeletePersonality).Methods("Delete") // The Delete method will delete the specified data
+	r.HandleFunc("/api/personalitys/{id}", controllers.EditPersonality).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
